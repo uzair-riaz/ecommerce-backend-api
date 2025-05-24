c7f8d2c309706ba112ea8cd4988d5ee1600e5131
Uzair Riaz Farooqui <uzair.r.farooqui@gmail.com>
2025-05-25 00:38:33 +0500

    setup express server with typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
